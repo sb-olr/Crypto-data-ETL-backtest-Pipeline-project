305ea8460aff30e89c97d3e5311698dfeef210d6
Sujit Badle <sujit.badle@nowtilus.tv>
2021-05-19 11:13:28 +0200

    initial commit

---
+++ 01-create-db.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+# db_name = ':memory:' # to make a in memory db
+db_name = 'crypto-data.db'
+
+# Connect to database; creates a new one if not existing
+conn = sqlite3.connect(db_name)
+
+# Create a cursor
+cur=conn.cursor()
+
+# 5 datatypes in SQLITE:
+# NULL, INTEGER, REAL, TEXT, BLOB
+
+# Create a table
+# https://stackoverflow.com/questions/4098008/create-table-in-sqlite-only-if-it-doesnt-exist-already
+sql_create = '''CREATE TABLE IF NOT EXISTS exchange_rates (
+    cur_id TEXT,
+    name TEXT,
+    unit TEXT,
+    value REAL,
+    type TEXT
+)'''
+cur.execute(sql_create)
+
+# Commit the changes
+conn.commit()
+
+# Close the connection
+conn.close()
