eb545bad6ab4cf0b427c5b69bf278685fa4e11ab
Sujit Badle <sujit.badle@nowtilus.tv>
2021-05-27 22:05:58 +0200

    add config file

--- 01-create-db.py
+++ 01-create-db.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # Usage: ./01-create-db.py
 import my_db_utils
+from config import CONFIG
 
 # db_name = ':memory:' # to make a in memory db
-db_name = 'data/TEST-crypto-db.sqlite3'
-table_name = 'exchange_rates'
-table_fields = ('cur_id TEXT', 'name TEXT', 'unit TEXT',
-                'value REAL', 'type TEXT', 'date TEXT')
+db_name = CONFIG['data_dir']+CONFIG['db_file']
+table_name = CONFIG['table_name']
+table_fields = CONFIG['table_fields']
+
 my_db_utils.create_table(db_name, table_name, table_fields)
---
+++ config.py
@@ -0,0 +1,7 @@
+CONFIG = {
+    'data_dir': 'data/',
+    'db_file': 'TEST-crypto-db.sqlite3',
+    'table_name':'exchange_rates',
+    'table_fields' : ('cur_id TEXT', 'name TEXT', 'unit TEXT',
+                'value REAL', 'type TEXT', 'date TEXT')
+}
